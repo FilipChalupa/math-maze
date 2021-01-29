8754b4ae9052222ca1b9c3edf5d2979f6a1f25db
Filip Chalupa <chalupa.filip@gmail.com>
2021-01-29 20:09:19 +0100

    Index: add multiple levels

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,67 @@
+import {
+	Avatar,
+	Container,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Typography,
+} from '@material-ui/core'
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports'
 import Link from 'next/link'
 import React from 'react'
-import s from '../styles/Home.module.css'
 
 export default function Home() {
 	return (
-		<div className={s.container}>
+		<Container maxWidth="xs">
+			<br />
+			<br />
+			<br />
+			<Typography variant="h4" gutterBottom>
+				Vyberte level
+			</Typography>
+			<List>
+				<Link href="/map?s=a" passHref>
+					<ListItem button component="a">
+						<ListItemAvatar>
+							<Avatar>
+								<SportsEsportsIcon />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText
+							primary="Úkázkový level 1"
+							secondary="sčítání, odčítání, násobení, dělení"
+						/>
+					</ListItem>
+				</Link>
+				<Link href="/map?s=b" passHref>
+					<ListItem button component="a">
+						<ListItemAvatar>
+							<Avatar>
+								<SportsEsportsIcon />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText
+							primary="Úkázkový level 2"
+							secondary="sčítání, odčítání, násobení, dělení"
+						/>
+					</ListItem>
+				</Link>
+				<Link href="/map?s=c" passHref>
+					<ListItem button component="a">
+						<ListItemAvatar>
+							<Avatar>
+								<SportsEsportsIcon />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText
+							primary="Úkázkový level 2"
+							secondary="sčítání, odčítání, násobení, dělení"
+						/>
+					</ListItem>
+				</Link>
+			</List>
+			{/*<div className={s.container}>
 			<ul>
 				<li>
 					<Link href="/map?s=asd">
@@ -17,6 +74,7 @@ export default function Home() {
 					</Link>
 				</li>
 			</ul>
-		</div>
+	</div>*/}
+		</Container>
 	)
 }
