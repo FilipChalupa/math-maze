f3279a09311e5e2f2a2a33be93ff6bed366b7ec5
Filip Chalupa <chalupa.filip@gmail.com>
2021-01-31 23:10:26 +0100

    Revert "Workbox: add modules"
    This reverts commit edd32d9568b97487b8f4bb4bcd4ccd05e8a90ad8.

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,6 @@ importScripts(
 	'https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js',
 )
 
-workbox.loadModule('workbox-precaching')
-workbox.loadModule('workbox-strategies')
-
-workbox.precaching.precacheAndRoute([])
-
 addEventListener('message', (event) => {
 	if (event.data && event.data.type === 'SKIP_WAITING') {
 		skipWaiting()
