b1845e02a57442c9058308c0d0959d8f56531713
Filip Chalupa <chalupa.filip@gmail.com>
2021-02-04 21:16:47 +0100

    Index: tabbed

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,11 +1,11 @@
 import { createMuiTheme, useMediaQuery } from '@material-ui/core'
 import deepPurple from '@material-ui/core/colors/deepPurple'
-import lime from '@material-ui/core/colors/lime'
+import green from '@material-ui/core/colors/green'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import * as React from 'react'
 
 export const primaryColor = deepPurple
-export const secondaryColor = lime
+export const secondaryColor = green
 export const themeColor = primaryColor[500]
 
 export const ThemeProvider: React.FunctionComponent = ({ children }) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import {
 	ListItem,
 	ListItemAvatar,
 	ListItemText,
+	Paper,
+	Tab,
+	Tabs,
 	Typography,
 } from '@material-ui/core'
 import AccountTreeIcon from '@material-ui/icons/AccountTree'
@@ -13,97 +16,126 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function Home() {
+	const [tabIndex, setTabIndex] = React.useState(0)
+	const handleTabIndexChange = React.useCallback(
+		(event: React.ChangeEvent<{}>, newValue: number) => {
+			setTabIndex(newValue)
+		},
+		[],
+	)
+
 	return (
-		<Container maxWidth="xs">
+		<>
+			<Paper square>
+				<Tabs
+					indicatorColor="primary"
+					textColor="inherit"
+					centered
+					value={tabIndex}
+					onChange={handleTabIndexChange}
+				>
+					<Tab label="Kolekce" icon={<AccountTreeIcon />} />
+					<Tab label="Levely" icon={<SportsEsportsIcon />} />
+				</Tabs>
+			</Paper>
 			<br />
 			<br />
-			<Typography variant="h4" gutterBottom>
-				Vyber kolekci
-			</Typography>
-			<List>
-				<Link href="/collection?i=tutorial" passHref>
-					<ListItem button component="a">
-						<ListItemAvatar>
-							<Avatar>
-								<AccountTreeIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText primary="Tutoriál" secondary="první krůčky" />
-					</ListItem>
-				</Link>
-				<Link href="/collection?i=example" passHref>
-					<ListItem button component="a">
-						<ListItemAvatar>
-							<Avatar>
-								<AccountTreeIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary="Ukázková kolekce"
-							secondary="sčítání, odčítání, násobení, dělení"
-						/>
-					</ListItem>
-				</Link>
-			</List>
-			<br />
-			<br />
-			<Typography variant="h4" gutterBottom>
-				Vyber level
-			</Typography>
-			<List>
-				<Link href="/map?i=a;8;5" passHref>
-					<ListItem button component="a">
-						<ListItemAvatar>
-							<Avatar>
-								<SportsEsportsIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary="Ukázkový level 1"
-							secondary="sčítání, odčítání, násobení, dělení"
-						/>
-					</ListItem>
-				</Link>
-				<Link href="/map?i=b;16;9" passHref>
-					<ListItem button component="a">
-						<ListItemAvatar>
-							<Avatar>
-								<SportsEsportsIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary="Ukázkový level 2"
-							secondary="sčítání, odčítání, násobení, dělení"
-						/>
-					</ListItem>
-				</Link>
-				<Link href="/map?i=c;30;20" passHref>
-					<ListItem button component="a">
-						<ListItemAvatar>
-							<Avatar>
-								<SportsEsportsIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary="Ukázkový level 3"
-							secondary="sčítání, odčítání, násobení, dělení"
-						/>
-					</ListItem>
-				</Link>
-				<Link href="/map?i=d;40;6" passHref>
-					<ListItem button component="a">
-						<ListItemAvatar>
-							<Avatar>
-								<SportsEsportsIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText
-							primary="Ukázkový level 4"
-							secondary="sčítání, odčítání, násobení, dělení"
-						/>
-					</ListItem>
-				</Link>
-			</List>
-		</Container>
+			<Container maxWidth="xs">
+				{tabIndex === 0 ? (
+					<>
+						<Typography variant="h4" gutterBottom>
+							Vyber kolekci
+						</Typography>
+						<List>
+							<Link href="/collection?i=tutorial" passHref>
+								<ListItem button component="a">
+									<ListItemAvatar>
+										<Avatar>
+											<AccountTreeIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText primary="Tutoriál" secondary="první krůčky" />
+								</ListItem>
+							</Link>
+							<Link href="/collection?i=example" passHref>
+								<ListItem button component="a">
+									<ListItemAvatar>
+										<Avatar>
+											<AccountTreeIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText
+										primary="Ukázková kolekce"
+										secondary="sčítání, odčítání, násobení, dělení"
+									/>
+								</ListItem>
+							</Link>
+						</List>
+					</>
+				) : tabIndex === 1 ? (
+					<>
+						<Typography variant="h4" gutterBottom>
+							Vyber level
+						</Typography>
+						<List>
+							<Link href="/map?i=a;8;5" passHref>
+								<ListItem button component="a">
+									<ListItemAvatar>
+										<Avatar>
+											<SportsEsportsIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText
+										primary="Ukázkový level 1"
+										secondary="sčítání, odčítání, násobení, dělení"
+									/>
+								</ListItem>
+							</Link>
+							<Link href="/map?i=b;16;9" passHref>
+								<ListItem button component="a">
+									<ListItemAvatar>
+										<Avatar>
+											<SportsEsportsIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText
+										primary="Ukázkový level 2"
+										secondary="sčítání, odčítání, násobení, dělení"
+									/>
+								</ListItem>
+							</Link>
+							<Link href="/map?i=c;30;20" passHref>
+								<ListItem button component="a">
+									<ListItemAvatar>
+										<Avatar>
+											<SportsEsportsIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText
+										primary="Ukázkový level 3"
+										secondary="sčítání, odčítání, násobení, dělení"
+									/>
+								</ListItem>
+							</Link>
+							<Link href="/map?i=d;40;6" passHref>
+								<ListItem button component="a">
+									<ListItemAvatar>
+										<Avatar>
+											<SportsEsportsIcon />
+										</Avatar>
+									</ListItemAvatar>
+									<ListItemText
+										primary="Ukázkový level 4"
+										secondary="sčítání, odčítání, násobení, dělení"
+									/>
+								</ListItem>
+							</Link>
+						</List>
+					</>
+				) : (
+					<></>
+				)}
+			</Container>
+		</>
 	)
 }
