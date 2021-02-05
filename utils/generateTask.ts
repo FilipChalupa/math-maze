b1aab91358dacb567da4bb053e4c52e8ae757ae5
Filip Chalupa <chalupa.filip@gmail.com>
2021-02-05 22:46:51 +0100

    Generate task: split into parts

--- utils/generateTask.ts
+++ utils/generateTask.ts
@@ -1,30 +1,42 @@
-export function generateTask(random: () => number) {
+import { assertNever } from './assertNever'
+function createTask(a: number, b: number, operator: '+' | '-' | '*' | '/') {
+	const solution = (() => {
+		switch (operator) {
+			case '+':
+				return a + b
+			case '-':
+				return a - b
+			case '*':
+				return a * b
+			case '/':
+				return a / b
+			default:
+				return assertNever(operator)
+		}
+	})().toString()
+	return {
+		label: `${a} ${operator} ${b}`,
+		solution,
+	}
+}
+
+export function generateTask(
+	random: () => number,
+): ReturnType<typeof createTask> {
 	const a = Math.floor(random() * 15)
 	const b = Math.floor(random() * 15)
 
 	if (a >= b && random() > 0.5) {
-		return {
-			label: `${a} - ${b}`,
-			solution: `${a - b}`,
-		}
+		return createTask(a, b, '-')
 	}
 
 	if (a <= 10 && b <= 10 && a !== 0 && random() < 0.9) {
-		if (b == 0 || random() > 0.5) {
-			return {
-				label: `${a} * ${b}`,
-				solution: `${a * b}`,
-			}
+		if (b === 0 || random() > 0.5) {
+			return createTask(a, b, '*')
 		}
 
-		return {
-			label: `${a * b} / ${b}`,
-			solution: `${a}`,
-		}
+		return createTask(a * b, b, '/')
 	}
 
-	return {
-		label: `${a} + ${b}`,
-		solution: `${a + b}`,
-	}
+	return createTask(a, b, '+')
 }
